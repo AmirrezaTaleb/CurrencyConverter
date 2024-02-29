f690e1ef52ad557571e1e845eb57f61a2eab662a
Amirreza Taleb <talebamirreza@gmail.com>
2024-02-29 20:48:26 +0330

    Add Test Project

---
+++ CurrencyConverter.Test/CurrencyConverterTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter.Test
+{
+     [TestFixture]
+    public class CurrencyConverterTests
+    {
+        private ICurrencyConverterServices converter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            converter = new CurrencyConverterServices();
+        }
+
+        [Test]
+        public void TestConvert_USD_to_EUR()
+        {
+            // Arrange
+            converter.UpdateConfiguration(new List<Tuple<string, string, double>>
+            {
+                Tuple.Create("USD", "CAD", 1.34),
+                Tuple.Create("CAD", "GBP", 0.58),
+                Tuple.Create("USD", "EUR", 0.86)
+            });
+
+            // Act
+            double result = converter.Convert("USD", "EUR", 100);
+
+            // Assert
+            Assert.AreEqual(86, result);
+        }
+
+        [Test]
+        public void TestConvert_CAD_to_GBP()
+        {
+            // Arrange
+            converter.UpdateConfiguration(new List<Tuple<string, string, double>>
+            {
+                Tuple.Create("USD", "CAD", 1.34),
+                Tuple.Create("CAD", "GBP", 0.58),
+                Tuple.Create("USD", "EUR", 0.86)
+            });
+
+            // Act
+            double result = converter.Convert("CAD", "GBP", 100);
+
+            // Assert
+            Assert.AreEqual(57.999999999999993d, result);
+        }
+
+        [Test]
+        public void TestConvert_InvalidCurrencies()
+        {
+            // Arrange
+            converter.UpdateConfiguration(new List<Tuple<string, string, double>>
+            {
+                Tuple.Create("USD", "CAD", 1.34),
+                Tuple.Create("CAD", "GBP", 0.58),
+                Tuple.Create("USD", "EUR", 0.86)
+            });
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => converter.Convert("EUR", "IR", 100));
+        }
+    }
+}
