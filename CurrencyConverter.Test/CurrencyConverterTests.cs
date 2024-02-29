3ddc156ffd0725fd699e47f64ade64091d487236
Amirreza Taleb <talebamirreza@gmail.com>
2024-02-29 20:54:34 +0330

    Add More Complex Tests

--- CurrencyConverter.Test/CurrencyConverterTests.cs
+++ CurrencyConverter.Test/CurrencyConverterTests.cs
@@ -52,7 +52,40 @@ namespace CurrencyConverter.Test
             // Assert
             Assert.AreEqual(57.999999999999993d, result);
         }
+        [Test]
+        public void TestConvert_EUR_to_USD()
+        {
+            // Arrange
+            converter.UpdateConfiguration(new List<Tuple<string, string, double>>
+            {
+                Tuple.Create("USD", "CAD", 1.34),
+                Tuple.Create("CAD", "GBP", 0.58),
+                Tuple.Create("USD", "EUR", 0.86)
+            });
 
+            // Act
+            double result = converter.Convert("EUR", "USD", 100);
+
+            // Assert
+            Assert.AreEqual(116.27906976744187, result);
+        }
+        [Test]
+        public void TestConvert_EUR_to_GBP()
+        {
+            // Arrange
+            converter.UpdateConfiguration(new List<Tuple<string, string, double>>
+            {
+                Tuple.Create("USD", "CAD", 1.34),
+                Tuple.Create("CAD", "GBP", 0.58),
+                Tuple.Create("USD", "EUR", 0.86)
+            });
+
+            // Act
+            double result = converter.Convert("USD", "GBP", 100);
+
+            // Assert
+            Assert.AreEqual(77.719999999999999d, result);
+        }
         [Test]
         public void TestConvert_InvalidCurrencies()
         {
